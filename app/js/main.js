70200283e50115f058b2b0c9caefff0f6ba9cd8e
irina_petrushenko <irina.aninger@gmail.com>
2019-10-16 14:09:48 +0300

    add projects

--- app/js/main.js
+++ app/js/main.js
@@ -2,59 +2,66 @@
 
 var btnModal = document.querySelector('.btn-modal'),
     modal    = document.querySelector('.contacts__modal'),
-    page     = $('html, body'),
+    page     = $("html, body"),
     navElem  = document.querySelectorAll('.header__nav-item');
 
-document.addEventListener('scroll', function () {
+document.addEventListener('scroll', function() {
     if (pageYOffset >= 100) {
-        document.querySelector('.header').classList.add('header_fixed')
+        document.querySelector('.header').classList.add('header_fixed');
     } else {
-        document.querySelector('.header').classList.remove('header_fixed')
+        document.querySelector('.header').classList.remove('header_fixed');
     }
 });
 
 $('a[href*="#"]').click(function() {
-    page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
+    page.animate(
+        {
+            scrollTop: $($.attr(this, 'href')).offset().top
+        },
+        400
+    );
     return false;
 });
 
-
 $('.header__btn').click(function() {
-    page.animate({
-        scrollTop: $('#contacts').offset().top
-    }, 400);
+    page.animate(
+        {
+            scrollTop: $('#contacts').offset().top
+        },
+        400
+    );
     modal.style.display = 'block';
     document.querySelector('.burger').classList.remove('active');
     document.querySelector('.header__nav').classList.remove('active');
     return false;
 });
 
-document.querySelector('.burger').addEventListener('click', function () {
+document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__nav').classList.toggle('active');
-    closeMenu()
+    closeMenu();
 });
 
 function closeMenu() {
-    navElem.forEach(function (el) {
-        el.addEventListener('click', function () {
+    navElem.forEach(function(el) {
+        el.addEventListener('click', function() {
             document.querySelector('.burger').classList.remove('active');
-            document.querySelector('.header__nav').classList.remove('active')
+            document.querySelector('.header__nav').classList.remove('active');
         });
-    })
+    });
 }
 
 $('.slider__item').slick({
     speed: 500
 });
 
-btnModal.addEventListener('click', function () {
-    modal.style.display = 'block'
+btnModal.addEventListener('click', function() {
+    modal.style.display = 'block';
 });
 
 document.querySelector('.close').addEventListener('click', closeModal);
+
 function closeModal() {
-    modal.style.display = 'none'
+    modal.style.display = 'none';
 }
+
